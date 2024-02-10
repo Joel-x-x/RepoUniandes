b09bd39389c3440893e0819cbcb7db9135db621c
Joel <acostajoel0001@gmail.com>
2024-02-09 20:19:50 -0500

    feat: add clases Usuario

--- proyecto1/config/conexion.php
+++ proyecto1/config/conexion.php
@@ -4,8 +4,27 @@ class Conexion {
   
   public $conexion;
   protected $bd;
-  private $servidor = "localhost:3306";
+  private $servidor = "localhost";
+  private $usuario = "root";
+  private $password = "";
+  private $nombreBaseDatos = "Quinto";
 
+  public function conectar() {
+    $this->conexion = mysqli_connect($this->servidor, $this->usuario, $this->password, $this->nombreBaseDatos);
+
+    mysqli_query($this->conexion, "SET NAMES 'utf-8'");
+
+    if(!$this->conexion) die ("Error al conectarse al servidor" . mysqli_error($this->conexion));
+
+    $this->bd = mysqli_select_db($this->conexion, $this->nombreBaseDatos);
+
+    if(!$this->bd) {
+      die("Error al conectarse con la base de datos" . mysqli_error($this->conexion));
+    };
+
+    return $this->conexion;
+  }
+  
 }
 
 ?>
